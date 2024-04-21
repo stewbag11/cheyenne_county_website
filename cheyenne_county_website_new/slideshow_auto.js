77138e0a6794cd62e098d1ce9e5533c1cd608c01
stewbag69 <spencerdennison@gmail.com>
2024-04-21 10:57:27 -0600

    First version control commit for Cheyenne County Website

---
+++ cheyenne_county_website_new/slideshow_auto.js
@@ -0,0 +1,15 @@
+// Auto slideshow
+let slideIndexAuto = 0;
+showSlidesAuto();
+
+function showSlidesAuto() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndexAuto++;
+  if (slideIndexAuto > slides.length) { slideIndexAuto = 1; }
+  slides[slideIndexAuto-1].style.display = "block";
+  setTimeout(showSlidesAuto, 8000); // Change image every 8 seconds
+}
---
+++ cheyenne_county_website_new/translate.js
@@ -0,0 +1,4 @@
+// Google translate api button
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
