4bbb8e9d2cd5b2ce1f6397b5b4749a9afa4db17a
stewbag69 <spencerdennison@gmail.com>
2024-05-07 18:37:38 -0600

    fix: small changes to back to top button and nesting for .htmls

--- README.md
+++ README.md
@@ -3,17 +3,14 @@
 **About page by Wednesday 5/8**
 
 ### To-Do
-- Jobs page (jobs.html)
 - Officals page (officals.html)
 - Board appointments (?)
 - Departments page (departments.html)
-- Back to Top button location change? (base.css)
 - Color switcher (normal, dark, invert, base.css)
 - Subnav for navs?
 - Search button or box? (base.css)
 - Breadcrumbs? (base.css)
 - Temperature meter? (base.css)
-- Slideshow animation? (index.css)
 - Media queries (all)
 - Regular development version and minified production version
 
--- cheyenne_county_website_new/top_btn.js
+++ cheyenne_county_website_new/top_btn.js
@@ -1,11 +1,11 @@
-// To top button
+// Get the button:
 let mybutton = document.getElementById("top-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -16,4 +16,4 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}  
+} 
