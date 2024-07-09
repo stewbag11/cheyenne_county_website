b81ec8346a28de867742fd14db764871a0cdf60d
stewbag69 <spencerdennison@gmail.com>
2024-07-09 02:28:21 -0600

    feat: hamburger menu and overall better mobile version for v2.0.0

---
+++ cheyenne_county_website/js/hamburger.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{const e=document.getElementById("menu__toggle"),t=document.querySelector(".menu__btn");t.addEventListener("click",t=>{t.preventDefault(),e.checked=!e.checked})});
